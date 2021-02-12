4433c30225198e9b279741226f7e26b0e4002e98
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2021-02-11 19:20:10 -1000

    Updated ListView

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
+from django.views.generic import ListView
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     object_list = Post.published.all()
