626b291e57a1c717524321ac69692c4a314ac67a
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2021-02-15 12:24:49 -1000

    Added comments to detail pages

--- blog/mysite/blog/models.py
+++ blog/mysite/blog/models.py
@@ -31,7 +31,7 @@ class Post(models.Model):
                               default='draft')
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
-    
+
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
@@ -42,3 +42,18 @@ class Post(models.Model):
                        args=[self.publish.year,
                              self.publish.month,
                              self.publish.day, self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
